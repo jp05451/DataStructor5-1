d49b1e50317ba3b5f82835a49f9bc9f951260d47
jp05451 <jp05451@gmail.com>
2022-01-12 03:11:05 +0800

    2022/01/12

--- MinimumSpanningForest.cpp
+++ MinimumSpanningForest.cpp
@@ -14,29 +14,47 @@ public:
 class minimumSpanningTree
 {
 public:
-    minimumSpanningTree(int);
+    minimumSpanningTree(int, int);
+    void inputNodes();
+    void prim();
+
     vector<vector<int>> treeMap;
+    vector<bool> visit;
+    int nodes;
+    int edges;
 };
+
+minimumSpanningTree::minimumSpanningTree(int nodes, int edges)
+{
+    this->nodes = nodes;
+    this->edges = edges;
+    treeMap.resize(nodes);
+    for (int i = 0; i < nodes; i++)
+    {
+        treeMap[i].resize(nodes, -1);
+    }
+    visit.resize(nodes, 0);
+}
+
+void minimumSpanningTree::inputNodes()
+{
+    for (int i = 0; i < edges; i++)
+    {
+        int firstNode, secondNode, weight;
+        cin >> firstNode >> secondNode >> weight;
+        treeMap[firstNode][secondNode] = weight;
+        treeMap[secondNode][firstNode] = weight;
+        
+    }
+}
+
+void minimumSpanningTree::prim()
+{
+}
 
 int main()
 {
     int nodes, edges;
     cin >> nodes >> edges;
-    minimumSpanningTree t(nodes);
-    for (int i = 0; i < edges;i++)
-    {
-        int firstNode, secondeNode;
-        cin >> firstNode >> secondeNode;
-        cin >> t.treeMap[firstNode][secondeNode];
-        
-    }
-}
-
-minimumSpanningTree::minimumSpanningTree(int nodes)
-{
-    treeMap.resize(nodes);
-    for (int i = 0; i < nodes; i++)
-    {
-        treeMap[i].resize(nodes);
-    }
+    minimumSpanningTree t(nodes, edges);
 }
